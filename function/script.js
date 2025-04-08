1c312e4172a7bd91a43e1b8709bef4e1e101c1a3
ADCR14 <160253285+ADCR14@users.noreply.github.com>
2025-04-08 10:15:13 -0300

    Add files via upload

---
+++ function/script.js
@@ -0,0 +1,29 @@
+
+function somar(num1, num2){
+    alert(num1 + num2);
+}
+
+function subtracao(num1, num2){
+    alert(num1 - num2);
+}
+
+function multiplicacao(num1, num2){
+    alert(num1 * num2);
+}
+
+function divisao(num1, num2){
+    alert(num1 / num2);
+}
+
+
+alert("Aplicativo para somar dois números");
+let primeiroNumero = number( Prompt("Digite o primeiro número"));
+let segundoNumero = number( Prompt("Digite o segundo número"));
+
+somar (primeiroNumero, segundoNumero);
+
+subtracao (primeiroNumero, segundoNumero);
+
+multiplicacao (primeiroNumero, segundoNumero);
+
+divisao (primeiroNumero, segundoNumero);
